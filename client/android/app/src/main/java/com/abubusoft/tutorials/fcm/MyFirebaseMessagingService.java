7ec7361124ba9f77c83e20512e971f00f2ab3ba9
xcesco <xcesco@gmail.com>
2019-09-14 23:07:02 +0200

    First commit

---
+++ client/android/app/src/main/java/com/abubusoft/tutorials/fcm/MyFirebaseMessagingService.java
@@ -0,0 +1,35 @@
+package com.abubusoft.tutorials.fcm;
+
+import android.app.NotificationManager;
+import android.content.Context;
+import android.media.RingtoneManager;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService
+{
+
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        super.onMessageReceived(message);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
+                .setContentTitle(message.getNotification().getTitle())
+                .setContentText(message.getNotification().getBody())
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setStyle(new NotificationCompat.BigTextStyle())
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(false);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+
+        Log.i("AAA","ricevuto messaggioooooo!! "+message.getNotification().getBody());
+    }
+}
