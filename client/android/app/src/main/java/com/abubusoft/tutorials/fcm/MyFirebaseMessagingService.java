bd4b7bae6dee5d85deb94584973d96c9d32fe8f8
xcesco <xcesco@gmail.com>
2019-09-15 15:41:00 +0200

    @wip

--- client/android/app/src/main/java/com/abubusoft/tutorials/fcm/MyFirebaseMessagingService.java
+++ client/android/app/src/main/java/com/abubusoft/tutorials/fcm/MyFirebaseMessagingService.java
@@ -1,8 +1,11 @@
 package com.abubusoft.tutorials.fcm;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.graphics.Color;
 import android.media.RingtoneManager;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
@@ -16,7 +19,19 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService
     @Override
     public void onMessageReceived(RemoteMessage message) {
         super.onMessageReceived(message);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "tutorialspoint_01";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+             NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notifications", NotificationManager.IMPORTANCE_HIGH);
+            // Configure the notification channel.
+            notificationChannel.setDescription("Sample Channel description");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
                 .setContentTitle(message.getNotification().getTitle())
                 .setContentText(message.getNotification().getBody())
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
@@ -25,10 +40,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setAutoCancel(false);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        notificationManager.notify(0, notificationBuilder.build());
+      notificationManager.notify(0, notificationBuilder.build());
 
         Log.i("AAA","ricevuto messaggioooooo!! "+message.getNotification().getBody());
     }
